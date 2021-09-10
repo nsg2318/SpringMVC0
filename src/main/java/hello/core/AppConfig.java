ba56816d2ef66e7a961ab1706c58f91f1c580314
sunba <nsg2318@naver.com>
2021-09-10 15:54:46 +0900

    스프링 컨테이너 전환, 빈조회 테스트 코드 작성

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //어떤 객체가 어떤 구현체를 장착하고 그럴지에 대한 고민은 다 config가 한다.
 //impl들은 이제 더이상 리포지토리와 디스카운트정책의 구현체에 의존하지 않고 config로 DI를 해준다.
+
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,22 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     //순수한 자바 메서드의 테스트코드 스프링 관련 X
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //ApplicationContext 가 빈이든 뭐든 다 관리해준다. 이 구문이 스프링 컨테이너에 빈으로 등록해준다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+                                         //AppConfig의 메서드이름
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+
 //        MemberService memberService = new MemberServiceImpl();
         Member member1 = new Member(1L, "MemberA", Grade.VIP);
         memberService.join(member1);
